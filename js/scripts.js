3f0ac120dc42b144c1194dbfff81be8f99c1598a
MoritzFranzke <moritzfranzke@gmail.com>
2021-06-08 22:24:48 +0200

    add javascript for burgermenu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Welcome to the console of my Portfolio-Website!");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
